c05a988412f94c1cd1d000b1fe2ac4eb5611f8c3
Lewis Needham <lewisn@userconversion.com>
2020-01-28 00:16:45 -0500

    Fixed nav open state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,14 @@
 import { TOGGLE_NAV } from '../constants/action-types';
 
 export const initialState = {
-  open: false,
+  navIsOpen: false,
 };
 
 export const rootReducer = (state = initialState, action) => {
   switch (action.type) {
     case TOGGLE_NAV:
       console.log('TOGGLE_NAV');
-      return Object.assign({}, state, { open: !state.open });
+      return Object.assign({}, state, { navIsOpen: !state.navIsOpen });
 
     default:
       return state;
