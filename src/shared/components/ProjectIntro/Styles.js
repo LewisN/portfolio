2a534fceda2dca767d607d33ff70244449d5c294
Lewis Needham <lewisn@userconversion.com>
2020-02-06 17:44:04 -0500

    Reduced title padding on ProjectIntro

--- src/shared/components/ProjectIntro/Styles.js
+++ src/shared/components/ProjectIntro/Styles.js
@@ -14,7 +14,7 @@ export const StyledTitle = styled.h1`
 
   @media (min-width: 768px) {
     font-size: 10rem;
-    padding-top: 10rem;
+    padding-top: 7rem;
     margin-bottom: 7rem;
     line-height: 9rem;
     letter-spacing: -0.3rem;
