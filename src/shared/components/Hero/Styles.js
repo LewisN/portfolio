66dfb0fae5172cfd78f903ae88eab8a008f78526
Lewis Needham <lewisn@userconversion.com>
2020-02-06 15:51:18 -0500

    Limited height of background animation to prevent it from overflowing onto text on long project descriptions

--- src/shared/components/Hero/Styles.js
+++ src/shared/components/Hero/Styles.js
@@ -3,4 +3,18 @@ import styled from 'styled-components';
 export const StyledHero = styled.section`
   background-color: #FFF;
   padding-top: 100px;
+
+  /* 
+   * Limit the height of Vanta to prevent it from
+   * spilling over onto paragraph text
+   */
+  canvas.vanta-canvas {
+    top: initial !important;
+    bottom: 0 !important;
+    max-height: 380px !important;
+
+    @media (min-width: 768px) {
+      max-height: 670px !important;
+    }
+  }
 `;
