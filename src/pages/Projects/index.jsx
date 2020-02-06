cf706768017b23b58da0e938d871ab5b28b7efde
Lewis Needham <lewisn@userconversion.com>
2020-02-06 15:48:35 -0500

    Added optional chaining to API response in order to prevent errors if it's not as expected

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -20,24 +20,24 @@ const Project = props => {
       </Fragment> : (
         <Fragment>
           <Helmet>
-            <title>Lewis Needham - {props.project.fields.name} Project</title>
-            <meta name="description" content={props.project.fields.technologyList ? `Technologies: ${props.project.fields.technologyList.join(' ')}` : 'Portfolio of Lewis Needham - Front End Developer. Experienced JavaScript (ES6+) and React developer based in Toronto, ON.'}/>
+            <title>Lewis Needham - {props?.project?.fields?.name} Project</title>
+            <meta name="description" content={props?.project?.fields?.technologyList ? `Technologies: ${props?.project?.fields?.technologyList.join(' ')}` : 'Portfolio of Lewis Needham - Front End Developer. Experienced JavaScript (ES6+) and React developer based in Toronto, ON.'}/>
           </Helmet>
           <Hero>
             <ProjectIntro
-              name={props.project.fields.name}
-              desc={props.project.fields.desc}
-              sideList={props.project.fields.technologyList}
-              buttons={props.project.fields.buttons}
+              name={props?.project?.fields?.name}
+              desc={props?.project?.fields?.desc}
+              sideList={props?.project?.fields?.technologyList}
+              buttons={props?.project?.fields?.buttons}
             />
           </Hero>
-          {props.project.fields.mediaEntries.map((media, index) => (
+          {props?.project?.fields?.mediaEntries?.map((media, index) => (
             <MediaLarge
               key={index}
-              contentType={media.fields.media.fields.file.contentType}
-              bgColor={media.fields.backgroundColor}
-              src={media.fields.media.fields.file.url}
-              alt={media.fields.media.fields.title}
+              contentType={media?.fields?.media?.fields?.file?.contentType}
+              bgColor={media?.fields?.backgroundColor}
+              src={media?.fields?.media?.fields?.file.url}
+              alt={media?.fields?.media?.fields?.title}
             />
           ))}
         </Fragment>
