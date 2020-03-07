a394c451992006a2c683e00c388f73f6ab2afc66
Lewis Needham <lewisn@userconversion.com>
2020-03-07 18:46:05 -0500

    Added single line returns to Profile component

--- src/shared/components/Profile/ProfileImage/index.jsx
+++ src/shared/components/Profile/ProfileImage/index.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { StyledProfileImage, StyledImage, StyledBackground } from './Styles';
 
-const ProfileImage = () => {
-  return (
-    <StyledProfileImage>
-      <StyledBackground />
-      <StyledImage img={require('../../../assets/profile.jpg')} />
-    </StyledProfileImage>
-  );
-};
+const ProfileImage = () => (
+  <StyledProfileImage>
+    <StyledBackground />
+    <StyledImage img={require('../../../assets/profile.jpg')} />
+  </StyledProfileImage>
+);
 
 export default ProfileImage;
--- src/shared/components/Profile/index.jsx
+++ src/shared/components/Profile/index.jsx
@@ -6,32 +6,30 @@ import {
 } from './Styles';
 import ProfileImage from './ProfileImage';
 
-const Profile = () => {
-  return (
-    <StyledProfile>
-      <StyledProfileSection>
-        <ProfileImage img={require('../../assets/profile.jpg')} />
-      </StyledProfileSection>
+const Profile = () => (
+  <StyledProfile>
+    <StyledProfileSection>
+      <ProfileImage img={require('../../assets/profile.jpg')} />
+    </StyledProfileSection>
 
-      <StyledProfileSection>
-        <StyledProfileContent>
-          <h3>About</h3>
-          <p>
-            I’m a front-end developer with a strong passion for solving problems and creating beautiful experiences. I have over 4 years experience, both as an individual contributor at a conversion rate optimization agency then later as the team lead. I have proven experience in building smooth user interfaces across a number of global brands and SMEs.
-          </p>
-          <h3>Technical Skills</h3>
-          <ul>
-            <li>JavaScript (ES6+)</li>
-            <li>React + Redux</li>
-            <li>AngularJS</li>
-            <li>Modern Build Tools (Webpack, Gulp, npm)</li>
-            <li>SASS/LESS</li>
-            <li>A/B Test Development</li>
-          </ul>
-        </StyledProfileContent>
-      </StyledProfileSection>
-    </StyledProfile>
-  );
-};
+    <StyledProfileSection>
+      <StyledProfileContent>
+        <h3>About</h3>
+        <p>
+          I’m a front-end developer with a strong passion for solving problems and creating beautiful experiences. I have over 4 years experience, both as an individual contributor at a conversion rate optimization agency then later as the team lead. I have proven experience in building smooth user interfaces across a number of global brands and SMEs.
+        </p>
+        <h3>Technical Skills</h3>
+        <ul>
+          <li>JavaScript (ES6+)</li>
+          <li>React + Redux</li>
+          <li>AngularJS</li>
+          <li>Modern Build Tools (Webpack, Gulp, npm)</li>
+          <li>SASS/LESS</li>
+          <li>A/B Test Development</li>
+        </ul>
+      </StyledProfileContent>
+    </StyledProfileSection>
+  </StyledProfile>
+);
 
 export default Profile;
